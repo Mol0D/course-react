fc033ced5bd65139657dbe4589158e697a9d02f6
Mol0D <moloduk99@gmail.com>
2018-12-15 13:58:12 +0200

    Error

--- src/App.js
+++ src/App.js
@@ -3,31 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
-import styled from 'styled-components'
+import styled from 'styled-components';
+
+import Header from './components/Header/header';
+import Menu from './components/Menu/menu';
+import Main from './components/Main/main';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <SayFullName name="Aleks" surname="Molodyk" link="vk.com"/>
-      <SayFullName name="Aleks" surname="Molodyk" link="vk.com"/>
-      <SayFullName name="Aleks" surname="Molodyk" link="vk.com"/>
+      <div className="App">
+          <Header/>
+          <Menu/>
+          <Menu/>
       </div>
-      
-    );
+    )
   }
 }
 
-function SayFullName(props){
-  return(
-      <div>
-          <h1>My name is {props.name}, surname is - {props.surname}</h1>
-          <a href={props.link}>Link to my profile</a>
-      </div>
-      
-  )
-}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,22 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function Hello(){
-    let phrase = 'World!'
-    return(
-        <h1>Hello {phrase}</h1>        
-    )
-}
 
-function SayFullName(props){
-    return(
-        <div>
-            <h1>My name is {props.name}, surname is - {props.surname}</h1>
-            <a href={props.link}>Link to my profile</a>
-        </div>
-        
-    )
-}
 ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
